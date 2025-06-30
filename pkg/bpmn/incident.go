6b8b166c80e0e5c4d542cd517cc30988652023ee
Adam Klíma <adam.klima@ibpm.cz>
2025-06-30 11:29:45 +0200

    fix(127): indicent now resolved same way as jobs

--- pkg/bpmn/incident.go
+++ pkg/bpmn/incident.go
@@ -27,17 +27,29 @@ func createNewIncidentFromToken(err error, token runtime.ExecutionToken, engine 
 }
 
 func (engine *Engine) ResolveIncident(ctx context.Context, key int64) (err error) {
+	incident, instance, err := handleIncident(ctx, engine, key, err)
+	if err != nil {
+		return err
+	}
+
+	// TODO: make sure that process instance is not running and if so modify currently running instance
+	engine.runProcessInstance(ctx, &instance, []runtime.ExecutionToken{incident.Token})
+	return nil
+}
+
+func handleIncident(ctx context.Context, engine *Engine, key int64, err error) (runtime.Incident, runtime.ProcessInstance, error) {
 	ctx, resoveIncidentSpan := engine.tracer.Start(ctx, fmt.Sprintf("incident:%d", key))
 	defer func() {
 		if err != nil {
 			resoveIncidentSpan.RecordError(err)
 			resoveIncidentSpan.SetStatus(codes.Error, err.Error())
 		}
+		resoveIncidentSpan.End()
 	}()
 
 	incident, err := engine.persistence.FindIncidentByKey(ctx, key)
 	if err != nil {
-		return errors.Join(newEngineErrorf("failed to find incident with key: %d", key), err)
+		return runtime.Incident{}, runtime.ProcessInstance{}, errors.Join(newEngineErrorf("failed to find incident with key: %d", key), err)
 	}
 
 	resoveIncidentSpan.SetAttributes(
@@ -47,12 +59,12 @@ func (engine *Engine) ResolveIncident(ctx context.Context, key int64) (err error
 	)
 
 	if incident.ResolvedAt != nil {
-		return errors.New("incident already resolved")
+		return runtime.Incident{}, runtime.ProcessInstance{}, errors.New("incident already resolved")
 	}
 
 	instance, err := engine.persistence.FindProcessInstanceByKey(ctx, incident.ProcessInstanceKey)
 	if err != nil {
-		return errors.Join(newEngineErrorf("failed to find process instance with key: %d", incident.ProcessInstanceKey), err)
+		return runtime.Incident{}, runtime.ProcessInstance{}, errors.Join(newEngineErrorf("failed to find process instance with key: %d", incident.ProcessInstanceKey), err)
 	}
 
 	batch := engine.persistence.NewBatch()
@@ -67,12 +79,8 @@ func (engine *Engine) ResolveIncident(ctx context.Context, key int64) (err error
 
 	err = batch.Flush(ctx)
 	if err != nil {
-		return errors.Join(newEngineErrorf("failed to complete incident with key: %d", key), err)
+		return runtime.Incident{}, runtime.ProcessInstance{}, errors.Join(newEngineErrorf("failed to complete incident with key: %d", key), err)
 	}
 
-	resoveIncidentSpan.End()
-
-	// TODO: make sure that process instance is not running and if so modify currently running instance
-	engine.runProcessInstance(ctx, &instance, []runtime.ExecutionToken{incident.Token})
-	return nil
+	return incident, instance, nil
 }

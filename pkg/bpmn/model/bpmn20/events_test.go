6cf424b107153f2de5077264861158fbba2fe18c
klajmajk <adaklima@gmail.com>
2025-09-18 15:50:23 +0200

    feat(108): boundary events now paring

---
+++ pkg/bpmn/model/bpmn20/events_test.go
@@ -0,0 +1,38 @@
+package bpmn20
+
+import (
+	"encoding/xml"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BoundaryEvent_Parses(t *testing.T) {
+	var definitions TDefinitions
+	var xmlData, err = os.ReadFile("../../test-cases/message-boundary-event-noninterrupting.bpmn")
+
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+	err1 := xml.Unmarshal(xmlData, &definitions)
+	if err1 != nil {
+		t.Fatalf("failed to unmarshal XML: %v", err)
+	}
+	assert.Equal(t, 1, len(definitions.Process.BoundaryEvent))
+
+	be := definitions.Process.BoundaryEvent[0]
+	assert.Equal(t, 1, len(be.Output))
+
+	assert.Equal(t, ElementTypeBoundaryEvent, be.GetType())
+
+	var m TMessageEventDefinition
+	if messageEventDefinition, ok := be.EventDefinition.(TMessageEventDefinition); ok {
+		m = messageEventDefinition
+	} else {
+		assert.Fail(t, "Expected TMessageEventDefinition")
+	}
+
+	assert.Equal(t, "simple-boundary-id", m.MessageRef)
+
+}

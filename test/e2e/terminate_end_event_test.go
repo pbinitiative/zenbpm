035d7b6540d18095a65f57c3bcabe81bbc7dd4f0
alisher <abdurahmanov@gmail.com>
2026-02-08 10:09:25 +0100

    - #364 Terminate end event  - better test info

--- test/e2e/terminate_end_event_test.go
+++ test/e2e/terminate_end_event_test.go
@@ -11,13 +11,16 @@ func TestTerminateEndEvent(t *testing.T) {
 	var instance public.ProcessInstance
 	definition, err := deployGetDefinition(t, "parallel_flow_with_terminate_end_task.bpmn", "parallel_flow_with_terminate_end_task")
 
-	t.Run("create process instance", func(t *testing.T) {
+	t.Run("create process instance with parallel flow and terminate end task on 1 end", func(t *testing.T) {
 		instance, err = createProcessInstance(t, definition.Key, map[string]any{})
 		assert.NoError(t, err)
 		assert.NotEmpty(t, instance.Key)
 	})
 
-	t.Run("read instance state", func(t *testing.T) {
+	// The overall process instance state should be 'COMPLETED', even though the 2nd flow was waiting on the task
+	// but the first flow hits the Terminate End Event and cancels all other active flows(tokens).
+	// See the visualization of 'parallel_flow_with_terminate_end_task.bpmn'.
+	t.Run("the process instance state should be COMPLETED", func(t *testing.T) {
 		fetchedInstance, err := getProcessInstance(t, instance.Key)
 		assert.NoError(t, err)
 		assert.Equal(t, public.ProcessInstanceStateCompleted, fetchedInstance.State)
